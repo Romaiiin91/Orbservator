3f8a3df72865ef877ac8f729925f876b5c4f1428
Romain <94060068+Romaiiin91@users.noreply.github.com>
2022-06-19 17:20:56 +0200

    Affichage vitesse

---
+++ PostTraitement/VitessePlotting.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+filename="log_11-06-2022--19h47m23.txt"
+
+fichier = open(filename, "r")
+lignes = fichier.readlines()
+fichier.close()
+
+VitesseX = [0]
+Temps = [0]
+
+for ligne in lignes:
+    if (ligne[0]=='V'):
+        V = ligne[10:-2].strip().split(',')
+        for i in range(len(V)):
+            V[i] = float(V[i])
+        
+        VitesseX.append(V[0])
+
+    if (ligne[0]=='T'):
+        T = ligne[6:-2].strip().split(',')
+        for i in range(len(T)):
+            T[i] = float(T[i])
+        Temps.append(round(Temps[-1] + np.array(T).sum(), 3))
+
+plt.figure(0)
+plt.plot(Temps, VitesseX)
+plt.show()
+
+    
